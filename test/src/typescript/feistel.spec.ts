7e0a6a903c7eb1e96c22266de9ec1821265e80ad
Cyril Dever <cdever@edgewhere.fr>
2020-01-16 00:40:19 +0100

    adaptation of crumbl's obfuscator to feistel cipher

---
+++ test/src/typescript/feistel.spec.ts
@@ -0,0 +1,27 @@
+import * as chai from 'chai'
+import 'mocha'
+chai.should()
+
+type Feistel = typeof import('../../..')
+const feistel = require('../../../lib/src/typescript/index') as Feistel
+
+describe('Cipher', () => {
+    describe('apply', () => {
+        it('should be deterministic', () => {
+            const cipher = new feistel.Cipher('8ed9dcc1701c064f0fd7ae235f15143f989920e0ee9658bb7882c8d7d5f05692', 10)
+            const found = cipher.apply('Edgewhere')
+
+            const expected = '3d7c0a0f51415a521054'
+            found.toString('hex').should.equal(expected)
+        })
+    })
+    describe('unapply', () => {
+        it('should be deterministic', () => {
+            const cipher = new feistel.Cipher('8ed9dcc1701c064f0fd7ae235f15143f989920e0ee9658bb7882c8d7d5f05692', 10)
+            const found = cipher.unapply(Buffer.from('3d7c0a0f51415a521054', 'hex'))
+
+            const expected = 'Edgewhere'
+            found.should.equal(expected)
+        })
+    })
+})
